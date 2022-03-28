0c30c0d4e9c645890e88dcaa795e29e953ef105e
williamkunhan <71636278+williamkunhan@users.noreply.github.com>
2022-03-28 10:56:52 -0400

    Update vs2.js
    add comment

--- js/vs2.js
+++ js/vs2.js
@@ -1,5 +1,5 @@
 
-
+// A helper function that helps to clean and count data
 function counter(arr) {
   const count = {};
 
@@ -10,6 +10,8 @@ function counter(arr) {
       count[element] = 1;
     }
   }
+
+  // clean data to d3 likes
   result = []
   for (const element of Object.values(count)) {
     result.push({ "count": element })
@@ -18,8 +20,10 @@ function counter(arr) {
   return result
 }
 
+// 3 ring dount chart 
 function multiDountChart(data) {
 
+  // clean and get the data d3 want for each ring
   const arrayColumn = (arr, n) => arr.map(x => x[n]);
 
   DnCounter = counter(arrayColumn(data, "numberOfChildrenSponsored"))
@@ -29,8 +33,12 @@ function multiDountChart(data) {
   PtCounter = counter(arrayColumn(data, "paymentType"))
   console.log(PtCounter)
 
+  // get clean data
   cleanedData = [DnCounter, SorCounter, PtCounter]
+  // set up colors for each ring
   colors = [d3.scaleOrdinal(d3.schemeCategory10), d3.scaleOrdinal(d3.schemeDark2), d3.scaleOrdinal(d3.schemeTableau10)]
+
+  // set up svg width height and etc
   var width = 1000;
   var height = 1000;
   var donutWidth = 75;
@@ -38,18 +46,24 @@ function multiDountChart(data) {
   var svg = d3.select('#vis-svg-1')
     .attr('width', width)
     .attr('height', height);
+  
+  // start a pie function to get the data
   var pie = d3.pie()
     .value(function (d) { return d.count; })
     .sort(null);
 
+  // recursion will generate 3 ring
   for (let i = 0; i < 3; i++) {
 
+    // get color 
     var color1 = colors[i]
 
+    // genereate image
     var svg1 = svg.append('g')
       .attr('transform', 'translate(' + (width / 2) +
         ',' + (height / 2) + ')');
 
+    // calculate angle
     var arc1 = d3.arc()
       .innerRadius(radius1 - donutWidth)
       .outerRadius(radius1);
@@ -60,6 +74,7 @@ function multiDountChart(data) {
 
     const arcLabel = d3.arc().innerRadius(labelRadius).outerRadius(labelRadius);
 
+    // generate the ring
     svg1.selectAll('path')
       .data(pie(cleanedData[i]))
       .enter()
@@ -69,6 +84,7 @@ function multiDountChart(data) {
         return color1(i);
       });
 
+    // // genereate label
     // svg1.append("g")
     //   .attr("font-family", "sans-serif")
     //   .attr("font-size", 10)
@@ -91,6 +107,7 @@ function multiDountChart(data) {
 
 }
 
+// the main function to start the data transport
 d3.csv('data/CircularRingVis.csv', function (d) {
   return {
     numberOfChildrenSponsored: d.DN,
